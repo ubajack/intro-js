da50f65277f472df8127eb135337236796ee9791
ubajack <jacques.bach@ubalia.fr>
2023-02-13 12:06:04 +0100

    Objects + functions

--- main.js
+++ main.js
@@ -29,6 +29,7 @@ console.log(ex);
 console.log(ex1);
 */
 
+// Création d'un objet
 const monObjet = {
   nom: 'Objet',
   qi: 200,
@@ -43,3 +44,41 @@ const monObjet = {
 console.log(monObjet);
 console.log(monObjet.nom);
 monObjet.bonjour();
+
+// Création d'un objet depuis une fonction
+function monObjetEnFonction() {
+  this.nom = 'Obj';
+  this.numero = 111;
+  this.bonjour = () => {
+    console.log(`Bonjour, je suis ${this.nom}`);
+  };
+}
+
+let instanceObjetEnFonction = new monObjetEnFonction();
+instanceObjetEnFonction.bonjour();
+
+// Fonction qui s'appelle elle-même
+(function () {
+  console.log("Cette fonction s'est appelée toute seule");
+
+  (function () {
+    console.log('Celle-ci aussi');
+  })();
+})();
+
+// Fonction de callback appelée après le timeout
+function callback() {
+  console.log('Fonction appelée en retard');
+}
+
+const bonjourEnRetard = (millis) => {
+  // La fonction setTimeout retarde l'exécution
+  setTimeout(callback, millis);
+};
+
+bonjourEnRetard(2000);
+
+console.log('Coucou');
+console.log('Coucou');
+console.log('Coucou');
+console.log('Coucou');
